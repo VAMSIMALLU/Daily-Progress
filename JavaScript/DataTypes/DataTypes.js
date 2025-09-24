ab1ff0b25e1904a183157846b6d39dbfa0d7fad5
VamsiM <vamsimallu46@gmail.com>
2025-09-24 17:50:01 +0530

    DataTypes of JS

---
+++ JavaScript/DataTypes/DataTypes.js
@@ -0,0 +1,50 @@
+// String
+var A = "this is a var"
+
+console.log(A)
+console.log(typeof A)
+// let B = 34
+
+// const C = true
+
+// console.log(A)
+
+// console.log(B)
+
+//  console.log(C)
+
+// number
+var m = 12.684
+
+console.log(m)
+console.log(typeof m)
+
+// boolean
+var x = true
+var y = false
+
+console.log(x)
+console.log(y)
+console.log(typeof x)
+
+// undefined
+var i
+
+console.log(i)
+
+// null
+var j=null
+
+console.log(j)
+
+
+
+var Apple = "10"
+console.log(Apple)
+
+let Banana = Apple
+
+console.log(Banana)
+
+const chs="block"
+console.log(chs)
