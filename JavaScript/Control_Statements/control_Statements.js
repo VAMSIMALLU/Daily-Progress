a47cc0c482a033beecf24fb9d3b0752ef01011d0
VamsiM <vamsimallu46@gmail.com>
2025-09-30 18:22:34 +0530

    if,if..else statements

---
+++ JavaScript/Control_Statements/control_Statements.js
@@ -0,0 +1,73 @@
+// Control Statements
+// when we have multiple conditions
+
+// if 
+// if else
+// switch
+
+
+// if
+// syntax:
+//if(condition){
+// Executes when the Condition is true} 
+
+
+var a = 50
+var b = 10
+
+if(a>b){
+console.log("a is greater than b")
+}
+
+
+// if else 
+
+// syntax:
+// if(condition){
+// Executes when the condition is true
+// } else {
+// Executes when the condition is false}
+
+
+var c = 30
+var d = 50
+
+if(c<d){
+    console.log("d is greater than c")
+} else {
+    console.log("c is greater than d")
+}
+
+
+var e = 60
+var f = 70
+
+if(e>f){
+    console.log("60 is greater than 70")
+} else{
+    console.log("60 is less than 70")
+}
+
+
+
+// if else ladder
+
+// if(condition){
+// executes when the condition is true
+// } else if(condition){
+// Executes when the condition is true
+// }else{
+// Executes when the condition is false
+// }
+
+var a = 0
+
+if(a>0){
+console.log("It is a positive number")
+} else if(a<0){
+    console.log("It is a negative number")
+} else{
+    console.log("It is a zero")
+}
+
+
