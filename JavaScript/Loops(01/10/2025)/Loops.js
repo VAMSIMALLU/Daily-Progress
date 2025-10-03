663b737f23c96b47097949600022a5cba4f1fe19
VamsiM <vamsimallu46@gmail.com>
2025-10-03 18:11:18 +0530

    for,while & do while loops

--- JavaScript/Loops(01/10/2025)/Loops.js
+++ JavaScript/Loops(01/10/2025)/Loops.js
@@ -1,9 +1,19 @@
 // Loops
 // Loops are used to do same functionality again and again 
 
+
 // Loops Types:
+
 // 1)Conditional Loops
 // 2)Unconditional Loops
+
+//Entry controlled 1st condition checking if condition true then functionality
+// minimum no. of iterations 0
+// for,while
+
+// Exit controlled 1st functionality next condition checking
+// minimum no. of iterations 1
+// do while
 
 // 1)Conditional Loops
 // a)for Loop
@@ -11,3 +21,82 @@
 // c)do while Loop
 // d)Infinity Loop
  
+
+// for Loop
+
+// condition is false then it wil not printed
+// for(let a =10;a<5;i++){
+// console.log(a)
+// }
+
+
+// print the 1st 10 even numbers
+
+// method - 1
+// for(let i=1;i<=10;i++){
+//     console.log(i*2)
+// }
+
+
+//  method - 2
+// for(let i=1;i<=20;i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+
+//  method - 3
+// for(let j=0;j<=20;j+=2){
+//     if(j>0)
+//        console.log(j)
+// } 
+
+
+// // method - 4
+// for(let k=2;k<=20;k+=2){
+//     console.log(k)
+// }
+
+
+
+// while loop 
+
+// syntax:
+// declaration 
+// while(condition){
+// stmts
+// updation
+// }
+
+
+// let a = 5
+// while(a<10){
+//     console.log(a)
+//     a++
+// }
+
+
+// Do While Loop
+// syntax:
+// do{
+// stmts
+// updation
+// }
+// while(condition)
+
+// var a = 10
+// do{
+//     console.log(a)
+//     a++
+// }
+// while(a<5)
+
+
+// condition true
+var b = 5
+do{
+    console.log(b)
+    b++
+}
+while(b<20)
