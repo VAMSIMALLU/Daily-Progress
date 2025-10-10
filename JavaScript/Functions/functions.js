d03801eb3666b456a3e80dc67c078ad98b346136
VamsiM <vamsimallu46@gmail.com>
2025-10-10 18:31:26 +0530

    arguments & rest_parameters

--- JavaScript/Functions/functions.js
+++ JavaScript/Functions/functions.js
@@ -52,64 +52,44 @@
 // parameters (or) place holders
 // parameters are used to store the values of the function declaration
 
-// function declaration
-function add(a,b){ //parameters
-    console.log(a+b)
-}
-// function calling
-add(2,3)  //arguments
 
 
-function add(a,b){ //parameters
-    console.log(a-b)
-}
-add(50,20) //arguments
-
-
-// Default parameter
-
-// one type
-function add(a=10,b){
+function abcd(a,b){
     console.log(a,b)
-    console.log(a+b)
 }
 
-add(5,6)
+abcd(10,20,30,40)  //in this function we have 4 arguments but 2 parameters only so,to get remaining  we have a two ways
+// i)arguments object E.S-5
+// ii)rest parameters  E.S-6
 
-// second type
-function add(a=9,b){
-    console.log(a,b)
-    console.log(a+b)
+
+// i)arguments object
+function fghi(f,g){
+    console.log(f,g)
+    console.log(arguments) 
 }
-add(undefined,7)
+fghi(60,70,80,90)
 
 
-// third type
-function add(a,b){
-    console.log(a,b)
-    console.log(a+b)
+
+// ii)Rest Parameter
+// rest parameter is also a parameter
+// how to define a rest parameter "...restparameter"
+// reset parameter must be the last parameter in the parameter list
+
+function restparameter(a,b,d,e,f,...c){
+    console.log(a,b,d,e,f)
+    console.log(c)
 }
-add() 
+restparameter(1,2,3,4,5,6,7,8,9,10,11,12)
 
 
-// fourth type
-function add(a,b=undefined){
-    console.log(a,b)
-    console.log(a+b)
+
+// Arrow function E.S-6
+
+var z = (val1,val2,val3,val4,...val5) => {
+    console.log(val1,val2)
+    console.log(val3,val4)
+    console.log(val5)
 }
-add(11,19)
-
-
-// return values from return function
-function multiply(a,b){
-    console.log(a*b)
-    return "multiplication"
-}
-multiply(100,300)
-
-// one type of return function
-console.log(multiply(10,20))
-
-// another type of return function
-var a = multiply(40,30) 
-console.log(a)
+z(100,200,300,400,500,600,700,800,900,1000)
