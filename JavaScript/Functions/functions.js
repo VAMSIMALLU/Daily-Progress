0d68c1e35c61f22dcda4cafada630770898dec7f
VamsiM <vamsimallu46@gmail.com>
2025-10-13 18:19:29 +0530

    Hoisting & closure of JS

--- JavaScript/Functions/functions.js
+++ JavaScript/Functions/functions.js
@@ -54,42 +54,110 @@
 
 
 
-function abcd(a,b){
-    console.log(a,b)
+// function abcd(a,b){
+//     console.log(a,b)
+// }
+
+// abcd(10,20,30,40)  //in this function we have 4 arguments but 2 parameters only so,to get remaining  we have a two ways
+// // i)arguments object E.S-5
+// // ii)rest parameters  E.S-6
+
+
+// // i)arguments object
+// function fghi(f,g){
+//     console.log(f,g)
+//     console.log(arguments) 
+// }
+// fghi(60,70,80,90)
+
+
+
+// // ii)Rest Parameter
+// // rest parameter is also a parameter
+// // how to define a rest parameter "...restparameter"
+// // reset parameter must be the last parameter in the parameter list
+
+// function restparameter(a,b,d,e,f,...c){
+//     console.log(a,b,d,e,f)
+//     console.log(c)
+// }
+// restparameter(1,2,3,4,5,6,7,8,9,10,11,12)
+
+
+
+// // Arrow function E.S-6
+
+// var z = (val1,val2,val3,val4,...val5) => {
+//     console.log(val1,val2)
+//     console.log(val3,val4)
+//     console.log(val5)
+// }
+// z(100,200,300,400,500,600,700,800,900,1000)
+
+// Rest Parameter: It is used to collect all remaining arguments in single array variable
+
+// Arguments: Arguments are the values which are given at time of function calling
+
+// Parameters : Parameters are the place holders which are used to store the values of function declaration
+
+
+// Hoisting
+
+// Accessing the memory values even before it's Execution
+
+// By using var
+console.log(a,b) //it will stored in global scope and undefined printed
+var a = 10
+var b = 20
+console.log(a,b)
+
+// By using let, const
+// console.log(A,B) //it will stored in temporary dead zone and reference error will be printed 
+let A = 100
+const B = "Hello"
+console.log(A,B)
+
+
+// function declartion using Hoisting
+// sum()
+
+// function sum(){
+//     console.log("1")
+// }
+
+// function expression using Hoisting 
+
+// k() //type error
+var k = function hai(){
+    console.log("hai")
+}
+k()
+
+
+// closure
+
+// Inner function remembers variables from outer function's scope
+
+function outer(){
+    let count = 0;
+    function inner(){
+        count++;
+        console.log(count)
+    }
+    return inner;
 }
 
-abcd(10,20,30,40)  //in this function we have 4 arguments but 2 parameters only so,to get remaining  we have a two ways
-// i)arguments object E.S-5
-// ii)rest parameters  E.S-6
+const counter = outer();
+counter();
+counter();
+counter();
 
 
-// i)arguments object
-function fghi(f,g){
-    console.log(f,g)
-    console.log(arguments) 
-}
-fghi(60,70,80,90)
+// Scope
+// Accessibility and lifetime of that variable
 
+// i)Global Scope
+// var
 
-
-// ii)Rest Parameter
-// rest parameter is also a parameter
-// how to define a rest parameter "...restparameter"
-// reset parameter must be the last parameter in the parameter list
-
-function restparameter(a,b,d,e,f,...c){
-    console.log(a,b,d,e,f)
-    console.log(c)
-}
-restparameter(1,2,3,4,5,6,7,8,9,10,11,12)
-
-
-
-// Arrow function E.S-6
-
-var z = (val1,val2,val3,val4,...val5) => {
-    console.log(val1,val2)
-    console.log(val3,val4)
-    console.log(val5)
-}
-z(100,200,300,400,500,600,700,800,900,1000)
+// ii)Block Scope
+// let, const
